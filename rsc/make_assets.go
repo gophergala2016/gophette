82c5a5aa202641815267c4a7de738d765023f1c0
Lars <lars.scheme@gmail.com>
2016-01-23 21:15:01 +0100

    Created Evil Doctor Barney Starsoup animation and added it to the asset maker. It is not used in the game, yet.

--- rsc/make_assets.go
+++ rsc/make_assets.go
@@ -24,22 +24,29 @@ func main() {
 
 	gophette, err := xcf.LoadFromFile("./gophette.xcf")
 	check(err)
+	barney, err := xcf.LoadFromFile("./barney.xcf")
+	check(err)
 
-	// create the collision information
-	collision := gophette.GetLayerByName("collision")
-	left, top := findTopLeftNonTransparentPixel(collision)
-	right, bottom := findBottomRightNonTransparentPixel(collision)
-	// scale the collision rect just like the images
-	left = int(0.5 + scale*float64(left))
-	top = int(0.5 + scale*float64(top))
-	right = int(0.5 + scale*float64(right))
-	bottom = int(0.5 + scale*float64(bottom))
-	width, height := right-left+1, bottom-top+1
-	line := fmt.Sprintf(
-		"var HeroCollisionRect = Rectangle{%v, %v, %v, %v}\n",
-		left, top, width, height,
-	)
-	constants.WriteString(line)
+	// create the collision information for Gophette and Barney
+	addCollisionInfo := func(canvas xcf.Canvas, variable string) {
+		collision := canvas.GetLayerByName("collision")
+		left, top := findTopLeftNonTransparentPixel(collision)
+		right, bottom := findBottomRightNonTransparentPixel(collision)
+		// scale the collision rect just like the images
+		left = int(0.5 + scale*float64(left))
+		top = int(0.5 + scale*float64(top))
+		right = int(0.5 + scale*float64(right))
+		bottom = int(0.5 + scale*float64(bottom))
+		width, height := right-left+1, bottom-top+1
+		line := fmt.Sprintf(
+			"var %v = Rectangle{%v, %v, %v, %v}\n",
+			variable,
+			left, top, width, height,
+		)
+		constants.WriteString(line)
+	}
+	addCollisionInfo(gophette, "HeroCollisionRect")
+	addCollisionInfo(barney, "BarneyCollisionRect")
 
 	// create the image resources
 	for _, layer := range []string{
@@ -49,18 +56,32 @@ func main() {
 		"run3",
 	} {
 		small := scaleImage(gophette.GetLayerByName(layer))
+		resources["gophette_left_"+layer] = imageToBytes(small)
+		resources["gophette_right_"+layer] = imageToBytes(imaging.FlipH(small))
+	}
 
-		buffer := bytes.NewBuffer(nil)
-		check(png.Encode(buffer, small))
-		resources["gophette_left_"+layer] = buffer.Bytes()
-
-		buffer = bytes.NewBuffer(nil)
-		check(png.Encode(buffer, imaging.FlipH(small)))
-		resources["gophette_right_"+layer] = buffer.Bytes()
+	for _, layer := range []string{
+		"stand",
+		"run1",
+		"run2",
+		"run3",
+		"run4",
+		"run5",
+		"run6",
+	} {
+		small := scaleImage(barney.GetLayerByName(layer))
+		resources["barney_left_"+layer] = imageToBytes(small)
+		resources["barney_right_"+layer] = imageToBytes(imaging.FlipH(small))
 	}
 
 	content := toGoFile(resources, string(constants.Bytes()))
 	ioutil.WriteFile("../resources.go", content, 0777)
+}
+
+func imageToBytes(img image.Image) []byte {
+	buffer := bytes.NewBuffer(nil)
+	check(png.Encode(buffer, img))
+	return buffer.Bytes()
 }
 
 func findTopLeftNonTransparentPixel(img image.Image) (x, y int) {
