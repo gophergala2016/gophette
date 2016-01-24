ba8c93329eff63b61af2cb68cfd57ed3b7cd6952
Lars <lars.scheme@gmail.com>
2016-01-24 18:39:14 +0100

    Changed the level.

--- level_editor/objects.go
+++ level_editor/objects.go
@@ -6,8 +6,8 @@ type LevelObject struct {
 }
 
 var LevelObjects = []LevelObject{
-	{217, 533, 1492, 42, true},
+	{214, 537, 1106, 42, true},
 	{596, 364, 142, 34, false},
 	{907, 261, 160, 36, false},
-	{1731, 689, 178, 49, false},
+	{1360, 688, 178, 49, false},
 }
